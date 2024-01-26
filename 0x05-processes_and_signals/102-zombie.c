33f60a6131583da66c682b9f90012078daed1cb4
rahma korish <rahmakorish@gmail.com>
2024-01-26 13:05:48 -0800

    creating zombie process

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -12,7 +12,7 @@ int main(void)
 {
 	pid_t child, child1, child2, child3, child4;
 
-	if ((child==fork()) == 0)
+	if ((child == fork()) == 0)
 	{
 		printf("Zombie process created, PID: %d\n", getpid());
 		exit(0);
