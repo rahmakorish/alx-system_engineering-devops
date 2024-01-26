666221839d0efb5b9340d93fa5e992b209cfe61a
rahma korish <rahmakorish@gmail.com>
2024-01-26 12:57:31 -0800

    creating zombie process

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+/**
+ *main-entry point
+ *Return: Always EXIT_SUCCESS.
+ **/
+int main(void)
+{
+	pid_t child;
+	
+	if ((child = fork()) == 0)
+	{
+		printf("Zombie process created, PID: %d\n", getpid());
+		exit(0);
+	}
+	else if (child > 0)
+		sleep(5);
+	else
+	{
+		perror("fork");
+		return 1;
+	}
+	return 0;
+}
+/**
+ *infinite_while-function
+ *Return:always EXit
+ **/
+int infinite_while(void)
+{
+	    while (1)
+		        {
+				        sleep(1);
+					    }
+	        return (0);
+}
