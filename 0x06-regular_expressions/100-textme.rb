bea93a50dcd59a492fbb2867d12b5184d0c88fac
rahma korish <rahmakorish@gmail.com>
2024-01-30 09:46:12 -0800

    advanced regex

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,4 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/[^(from) a-z0-9]/).join
+puts ARGV[0].scan(/\bfrom:\b.{11}/).join
+puts ARGV[0].scan(/\bto:\b\d*/).join
+puts ARGV[0].scan(/\bflags:\b.{20}/).join
