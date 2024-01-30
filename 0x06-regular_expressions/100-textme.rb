04cac81293704160d2765e913ff0a5178616bc9d
rahma korish <rahmakorish@gmail.com>
2024-01-30 10:08:42 -0800

    advanced regex

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\bfrom:(.{11})\b|\bto:(\d*)\b|flags:(.{13})/).flatten.compact.join(',')
+puts ARGV[0].scan(/\bfrom:(.?.{11})\b|\bto:(.?\d*)\b|flags:(.{12,13})/).flatten.compact.join(',')
 #puts ARGV[0].scan(/\bto:(\d*)\b/).join
 #puts ARGV[0].scan(/flags:(.{13})/).join
