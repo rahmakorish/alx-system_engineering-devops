b1fa52e97f3c8da80e5394f90f8742479d2fa417
rahma <rahmakorish@gmail.com>
2024-03-22 13:37:39 +0200

    execute command using puupet

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,4 @@
 # execute command using Puppet
 exec { 'killmenow':
     command => 'pkill -x killmenow',
-    cwd     => '/home/rahma/alx-system_engineering-devops/0x0A-configuration_management/killmenow',
 }
